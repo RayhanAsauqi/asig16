85f45ef3c7b4957e139ebe13e151367ce2e8c43f
RayhanAsauqi <reyhanalsauqi12@gmail.com>
2023-12-02 19:11:52 +0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <div className='bg-[#140016]'>
+        <div>
+          <img src='@/src/app/img/asigName.svg' alt="" />
+        </div>
+      </div>
+    </>
+  )
+}
